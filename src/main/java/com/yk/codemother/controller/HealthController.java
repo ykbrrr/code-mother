3ef40ff1d339a47f99938d3b86492c77eeec02b8
ykbrrr <2981634913@qq.com>
2026-01-15 22:13:21 +0800

    Intial commit

---
+++ src/main/java/com/yk/codemother/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.yk.codemother.controller;
+
+import com.yk.codemother.common.BaseResponse;
+import com.yk.codemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
